1bf4a747ca577f847d49c06499ee44ca6e83c8a6
ageu_dev19 <ageumenezes423@gmail.com>
2024-01-01 15:55:18 -0300

    Start web app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Habit } from "./components/Habit";
+
+export function App() {
+  return (
+    <>
+      <Habit completed={5} />
+      <Habit completed={5} />
+      <Habit completed={5} />
+      <Habit completed={5} />
+      <Habit completed={5} />
+    </>
+  )
+}
